a3e5e363f764b8810e8fc27fb9a5a3204c86e96b
Alexandra Hermann <github@dev.hermann-bsd.de>
2021-02-06 22:22:32 +0100

    * Program.cs: developping
    * Test.csproj: added library to test and the library ahbsd.lib from
      nuget

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using ahbsd.lib.Nutrients.Data;
+using System.Data;
+using System.Data.SQLite;
 
-namespace Test
+namespace ahbsd.lib.Nutrients.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SQLiteErrorCode liteErrorCode;
+            Console.WriteLine("Test");
+            Console.WriteLine("====");
+
+            NutritionData data = new NutritionData();
+
+            data.Connection.StateChange += Connection_StateChange;
+
+            data.Connection.Open();
+
+
+            data.Connection.Close();
+
+            try
+            {
+                liteErrorCode = data.Connection.Shutdown();
+
+                Console.WriteLine($"Shutdown: {liteErrorCode}");
+            }
+            catch (Exception)
+            { }
+
+            data.Dispose();
+
+        }
+
+        private static void Connection_StateChange(object sender, StateChangeEventArgs e)
+        {
+            Console.WriteLine($"{sender}: Connection changed from '{e.OriginalState}' to '{e.CurrentState}'.");
         }
     }
 }
